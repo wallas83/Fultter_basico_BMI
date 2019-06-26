82eafd6b10e3713a18df38d18270911ad0fb789b
wilson <whuanca06@gmail.com>
2019-06-26 13:50:54 -0400

    adding functionality to bmi

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -7,6 +7,49 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final TextEditingController _ageController = new TextEditingController();
+  final TextEditingController _heightController = new TextEditingController();
+  final TextEditingController _weightController = new TextEditingController();
+  double cent = 0.0;
+
+  double bmi = 0.0;
+  String _resulReading = "";
+  String _finalResult = "";
+
+  void calculateIbm() {
+    setState(() {
+      int age = int.parse(_ageController.text);
+      double height = double.parse(_heightController.text);
+      cent = height * 100;
+      double weight = double.parse(_weightController.text);
+      if ((_ageController.text.isNotEmpty || age > 0) &&
+              (_heightController.text.isNotEmpty || height > 0) &&
+              (_weightController.text.isNotEmpty) ||
+          weight > 0) {
+        bmi = (weight / (cent * cent)) * 10000;
+        print(bmi);
+
+        if (double.parse(bmi.toStringAsFixed(1)) < 18.1) {
+          _resulReading = "Underweight";
+          print(_resulReading);
+          print("  asd  asd $bmi");
+        } else if (double.parse(bmi.toStringAsFixed(1)) >= 18.5 && bmi < 25) {
+          _resulReading = "Great Shape!";
+          print(_resulReading);
+        } else if (double.parse(bmi.toStringAsFixed(1)) >= 25.0 && bmi < 30) {
+          _resulReading = "Overweight";
+          print(_resulReading);
+        } else if (double.parse(bmi.toStringAsFixed(1)) >= 30.0) {
+          _resulReading = "Obese";
+          print(_resulReading);
+        }
+      } else {
+        bmi = 0.0;
+      }
+    });
+    _finalResult = "Your BMI. ${bmi.toStringAsFixed(1)}";
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -15,7 +58,6 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         backgroundColor: Colors.pinkAccent,
       ),
-
       body: new Container(
         alignment: Alignment.topCenter,
         child: new ListView(
@@ -29,10 +71,11 @@ class _HomeState extends State<Home> {
             new Container(
               margin: const EdgeInsets.all(2.0),
               alignment: Alignment.center,
+              color: Colors.grey.shade300,
               child: new Column(
                 children: <Widget>[
                   new TextField(
-                    controller: null,
+                    controller: _ageController,
                     keyboardType: TextInputType.number,
                     decoration: new InputDecoration(
                       labelText: "Age",
@@ -40,30 +83,25 @@ class _HomeState extends State<Home> {
                       icon: new Icon(Icons.person_outline),
                     ),
                   ),
-
                   new TextField(
-                    controller: null,
+                    controller: _heightController,
                     keyboardType: TextInputType.number,
                     decoration: new InputDecoration(
-                      labelText: "height in feet",
-                      hintText: "digit your heiht",
-                      icon:  new Icon(Icons.assessment)
-                    ),
+                        labelText: "height in feet",
+                        hintText: "digit your heiht",
+                        icon: new Icon(Icons.assessment)),
                   ),
                   new TextField(
-                  controller: null,
-                  keyboardType: TextInputType.number,
-                  decoration: new InputDecoration(
-                    labelText: "Weight in lb",
-                    hintText: "digit your weight",
-                    icon: new Icon(Icons.dehaze)
+                    controller: _weightController,
+                    keyboardType: TextInputType.number,
+                    decoration: new InputDecoration(
+                        labelText: "Weight in lb",
+                        hintText: "digit your weight",
+                        icon: new Icon(Icons.dehaze)),
                   ),
-                  ),
-
                   new Padding(
                     padding: new EdgeInsets.all(12.0),
                   ),
-
                   new RaisedButton(
                     color: Colors.pinkAccent,
                     textColor: Colors.white,
@@ -71,30 +109,39 @@ class _HomeState extends State<Home> {
                       "Calculate",
                       style: TextStyle(
                         fontSize: 20.0,
-
                       ),
                     ),
-                    onPressed: ()=> print("pulsado"),
+                    onPressed: () => calculateIbm(),
                   ),
-                  new Padding(
-                    padding: new EdgeInsets.all(12.0),
-                  ),
-                  new Text(
-                    "salida de IBM calculado",
-                        style: new TextStyle(
-                          color: Colors.blue,  fontWeight: FontWeight.w900,fontSize: 20.0),
-                  ),
-                  new Padding(
-                    padding: new EdgeInsets.all(12.0),
-                  ),
-                  new Text(
-                    "salidaa de ressultado",
-                    style: new TextStyle(
-                        color: Colors.pinkAccent,  fontWeight: FontWeight.w400),
-                  )
                 ],
               ),
             ),
+            new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Padding(
+                  padding: new EdgeInsets.all(12.0),
+                ),
+                new Text(
+                  " $_finalResult",
+                  style: new TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w900,
+                      fontSize: 20.0,
+                      fontStyle: FontStyle.italic),
+                ),
+                new Padding(
+                  padding: new EdgeInsets.all(12.0),
+                ),
+                new Text(
+                  "$_resulReading",
+                  style: new TextStyle(
+                      color: Colors.pinkAccent,
+                      fontWeight: FontWeight.w500,
+                      fontStyle: FontStyle.italic),
+                )
+              ],
+            )
           ],
         ),
       ),
